7f650844ce43fa21efe22c2088b3e14787e34c5e
Yasumasa Suenaga <yasuenag@gmail.com>
2015-03-16 12:53:43 +0900

    Version 0.1.1 release.

--- src/jp/dip/ysfactory/cfa/Option.java
+++ src/jp/dip/ysfactory/cfa/Option.java
@@ -62,7 +62,7 @@ public class Option{
    * Print usage.
    */
   public static void printOptions(){
-    System.out.println("Class File Analyzer (CFA)  development version");
+    System.out.println("Class File Analyzer (CFA)  0.1.1");
     System.out.println("Copyright (C) 2015 Yasumasa Suenaga");
     System.out.println();
     System.out.println("Usage:");
