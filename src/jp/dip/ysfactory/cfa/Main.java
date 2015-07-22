5b8817bb9eecefc0759469b24a71a6a5e7d42cd3
Yasumasa Suenaga <yasuenag@gmail.com>
2015-07-22 23:23:10 +0900

    Remove Bootstrap class.

--- src/jp/dip/ysfactory/cfa/Main.java
+++ src/jp/dip/ysfactory/cfa/Main.java
@@ -18,10 +18,38 @@ package jp.dip.ysfactory.cfa;
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
+import java.io.File;
+import java.nio.file.Paths;
+import java.lang.reflect.Method;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.io.FileNotFoundException;
+import java.lang.NoSuchMethodException;
+import java.net.MalformedURLException;
+import java.lang.IllegalAccessException;
+import java.lang.reflect.InvocationTargetException;
+
 
 public class Main{
 
+  private static void setupClassLoader() throws FileNotFoundException,
+                              NoSuchMethodException, MalformedURLException,
+                              IllegalAccessException, InvocationTargetException{
+    File tools_jar = Paths.get(System.getProperty("java.home"), "..",
+                                                  "lib", "tools.jar").toFile();
+    if(!tools_jar.exists()){
+      throw new FileNotFoundException("tools.jar does not exist.");
+    }
+
+    Method addURLMethod = URLClassLoader.class.getDeclaredMethod(
+                                                         "addURL", URL.class);
+    addURLMethod.setAccessible(true);
+    addURLMethod.invoke(Main.class.getClassLoader(), tools_jar.toURI().toURL());
+  }
+
   public static void main(String[] args) throws Exception{
+    setupClassLoader();
+
     Option option;
     try{
       option = new Option(args);
